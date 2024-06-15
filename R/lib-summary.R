d36b4cdf895fa4d8c305b77ec88e8598a95ee3a8
Rajendra K. Labala <rajendra.labala@gmail.com>
2024-06-15 15:09:00 +0200

    first commit

---
+++ R/lib-summary.R
@@ -0,0 +1,17 @@
+#' Title
+#'
+#'Provides a brief summary of the package libraries on your machine
+#'
+#' @return A data.frame containing the count of packages in each of the user's
+#'   libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
+lib_summary <- function() {
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+}
